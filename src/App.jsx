cd72643e64c353a855f36fcd690a5023bc490351
Mehul Poshattiwar <mehulposhattiwar4995@gmail.com>
2025-05-08 10:51:49 +0530

    calculator code

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,57 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
+
+const buttons = ["7", "8", "9", "/", "4", "5", "6", "*", "1", "2", "3", "-", "C", "0", "=", "+"];
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [expresion, setExpresion] = useState('');
+  const[result,setResult] = useState(null);
+  
+
+  const handleClick = (value) => {
+    if(value === 'C') {
+      setExpresion('');
+      setResult(null);
+    }
+    else if(value === '=') {
+      try {
+        if(expresion.trim() === ''){
+          setResult('Error');
+        }
+        else{
+          const evaluation = eval(expresion);
+          setResult(evaluation);
+        }
+      }
+      catch (error) {
+        setResult('Error');
+        console.error('Error evaluating expression:', error);
+      }
+        
+  }
+  else{
+    setExpresion((prev) => prev + value);
+  }
+  };
+
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='calculator'>
+    <h1>React Calculator</h1>
+    <input type="text" value={expresion} readOnly className="input-bar" />
+    <div className="result">{result !== null ? result : ""}</div>
+    <div className='buttons'>
+      {buttons.map((button) => (
+        <button key={button} onClick={() => handleClick(button)}>{button}</button>
+      ))}
+    </div>
+       
+    </div>
   )
+
 }
 
 export default App
